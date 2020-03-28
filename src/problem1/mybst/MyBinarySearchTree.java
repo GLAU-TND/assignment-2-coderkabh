e7938013cb7d23848bdfdac44b68a81cdd6c5739
kanishk <kanishkbharadwaj333@gmail.com>
2020-03-28 23:56:43 +0530

    make constructor of class MyBinarySearchTree.java

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,7 +5,18 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
+
+import problem1.node.TreeNode;
+
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
-    
+
+    public static StringBuilder preOrderString;
+    public TreeNode root;
+    private int count = 0;
+
+    public MyBinarySearchTree() {
+        this.root = null;
+        preOrderString = new StringBuilder();
+    }
 }
