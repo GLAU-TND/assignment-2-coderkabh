490ada445087b0284491918340b9c8a5af767201
kanishk <kanishkbharadwaj333@gmail.com>
2020-03-29 00:18:54 +0530

    complete class MyMain.java

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -12,18 +12,46 @@ import problem1.mybst.MyBinarySearchTree;
 import java.util.Scanner;
 
 public class MyMain {
-    MyBinarySearchTree myBinarySearchTree = new MyBinarySearchTree();
-    Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        MyBinarySearchTree myBinarySearchTree = new MyBinarySearchTree();
+        Scanner scan = new Scanner(System.in);
 
-    int i = 7;
+        int i = 7;
         System.out.println("Please take only 7 inputs");
         while (i > 0) {
-        try {
-            myBinarySearchTree.add(scan.nextInt());
-        } catch (Exception e) {
-            System.out.println("Error Occurred is \" " + e + " \", so you are going to be kicked-out \n");
-            break;
+            try {
+                myBinarySearchTree.add(scan.nextInt());
+            } catch (Exception e) {
+                System.out.println("Error Occurred: " + e);
+                break;
+            }
+            i--;
         }
-        i--;
+        System.out.println("All Left-children are :");
+        myBinarySearchTree.printLeftNode();
+        System.out.println("Number of nodes doesn't have left child :");
+        myBinarySearchTree.countNotLeft();
     }
+
+     /*
+        *
+        *
+        * > tree structure is:
+        *
+                       6
+                     /   \
+                    1     8
+                   / \    / \
+                  5   1  2  3
+        * Input :> 6
+                   1
+                   8
+                   5
+                   1
+                   2
+                   3
+        *
+        */
+
+
 }
