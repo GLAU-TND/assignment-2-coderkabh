61e4ea3f3b9c49301ba674ea784d1c08d9b821e0
kanishk <kanishkbharadwaj333@gmail.com>
2020-04-01 00:01:24 +0530

    add method insert()

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -5,7 +5,34 @@
  *  Time: 7:06 PM
  */
 package problem5.circularqueue;
+
+
+import problem5.node.Node;
+import problem5.student.Student;
+
 //to implement circular queue
 public class MyCircularQueue {
+
+    Node front;
+    Node rear;
+
+    public MyCircularQueue() {
+        rear = front = null;
+    }
+
+    public void insert(Student student) {
+        if (student.getAppearing() > 2)
+            System.out.println("Can't add this student to Queue : " + student.getName());
+        else {
+            Node node = new Node(student);
+            if (front == null) {
+                front = node;
+            } else
+                rear.setNext(node);
+            rear = node;
+            rear.setNext(front);
+        }
+    }
+
 
 }
